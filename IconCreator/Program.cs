efed4ee19515e3a49807591e12b83bb253542bb2
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2020-08-09 20:13:23 +0530

    fixes

--- IconCreator/Program.cs
+++ IconCreator/Program.cs
@@ -12,21 +12,21 @@ namespace IconCreator
     {
         static void Main(string[] args)
         {
-            var logo = Image.FromFile(@"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\icon_applogo_org.png");
+            var logo = Image.FromFile(@"D:\WhoKnows\Courses\Applications\WorkRegister\icons\icon_applogo_org.png");
 
-            saveResizeImage(logo, new Size(72, 72), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-hdpi\icon.png");
-            saveResizeImage(logo, new Size(48, 48), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-mdpi\icon.png");
-            saveResizeImage(logo, new Size(96, 96), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-xhdpi\icon.png");
-            saveResizeImage(logo, new Size(144, 144), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-xxhdpi\icon.png");
-            saveResizeImage(logo, new Size(192, 192), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-xxxhdpi\icon.png");
+            saveResizeImage(logo, new Size(72, 72), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-hdpi\icon.png");
+            saveResizeImage(logo, new Size(48, 48), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-mdpi\icon.png");
+            saveResizeImage(logo, new Size(96, 96), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-xhdpi\icon.png");
+            saveResizeImage(logo, new Size(144, 144), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-xxhdpi\icon.png");
+            saveResizeImage(logo, new Size(192, 192), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-xxxhdpi\icon.png");
 
-            var foreground = Image.FromFile(@"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\icon_foreground_org.png");
+            var foreground = Image.FromFile(@"D:\WhoKnows\Courses\Applications\WorkRegister\icons\icon_foreground_org.png");
 
-            saveResizeImage(foreground, new Size(162, 162), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-hdpi\launcher_foreground.png");
-            saveResizeImage(foreground, new Size(108, 108), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-mdpi\launcher_foreground.png");
-            saveResizeImage(foreground, new Size(216, 216), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-xhdpi\launcher_foreground.png");
-            saveResizeImage(foreground, new Size(324, 324), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-xxhdpi\launcher_foreground.png");
-            saveResizeImage(foreground, new Size(432, 432), @"D:\WhoKnows\Courses\Applications\BusinessAnalyst\icons\resizeImages\mipmap-xxxhdpi\launcher_foreground.png");
+            saveResizeImage(foreground, new Size(162, 162), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-hdpi\launcher_foreground.png");
+            saveResizeImage(foreground, new Size(108, 108), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-mdpi\launcher_foreground.png");
+            saveResizeImage(foreground, new Size(216, 216), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-xhdpi\launcher_foreground.png");
+            saveResizeImage(foreground, new Size(324, 324), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-xxhdpi\launcher_foreground.png");
+            saveResizeImage(foreground, new Size(432, 432), @"D:\WhoKnows\Courses\Applications\WorkRegister\icons\resizeImages\mipmap-xxxhdpi\launcher_foreground.png");
 
 
         }
